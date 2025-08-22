596e373d01c53b64acf03ce1b7480afa5f6eb614
Никита Бакутов <89133760758poi@gmail.com>
2025-08-22 08:42:33 +0700

    add lazy import

--- safire/__init__.py
+++ safire/__init__.py
@@ -1,7 +1,8 @@
-from . import jailbreaking
-from . import evaluation
+import importlib
 
-__all__ = [
-    'jailbreaking',
-    'evaluation'
-]
+__all__ = ['jailbreaking', 'evaluation']
+
+def __getattr__(name):
+    if name in __all__:
+        return importlib.import_module(f'{__name__}.{name}')
+    raise AttributeError(f'module {__name__} has no attribute {name}')
